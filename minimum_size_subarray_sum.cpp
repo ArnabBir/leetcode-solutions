2dc40673bfe9180c7148bb156e3bad256b707b08
Arnab Bir <arnabbir@gmail.com>
2021-06-03 10:37:38 +0530

    Create minimum_size_subarray_sum.cpp

---
+++ minimum_size_subarray_sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        int ans = INT_MAX;
+        int l = 0, r = 0;
+        while(r < n){
+            sum += nums[r];
+            while(sum >= target)  {
+                    ans = min(ans, r-l+1);
+                    sum -= nums[l++];
+            }
+            ++r;
+        }
+        return (ans == INT_MAX) ? 0 : ans;
+    }
+};
