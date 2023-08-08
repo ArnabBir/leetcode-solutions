0a03c99cf18f939d9be686581a31775501f5fd22
Arnab Bir <arnabbir@gmail.com>
2023-08-09 01:06:16 +0530

    updated solution

--- next_permutation.cpp
+++ next_permutation.cpp
@@ -1,28 +1,23 @@
+// Link : https://leetcode.com/problems/next-permutation/submissions
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        if(n == 0 || n == 1)    return;
-        if(n == 2) {
-            swap(nums[0], nums[1]);
-            return;
-        }
-        int x = n-2;
-        while(x >= 0) {
-            if(nums[x] < nums[x+1]) {
-                int miny = x+1;
-                for(int y = x+1; y < n; ++y) {
-                    if(nums[x] < nums[y]) {
-                        if(nums[miny] > nums[y]) {
-                            miny = y;
-                        }
-                    }
-                }
-                swap(nums[x], nums[miny]);
+        int i;
+        for (i = n-1; i > 0; --i) {
+            if (nums[i] > nums[i-1]) {
                 break;
             }
-            --x;
         }
-        sort(nums.begin()+x+1, nums.end());
+        sort(nums.begin() + i, nums.end());
+        if (i > 0) {
+            for (int j = i; j < n; ++j) {
+                if (nums[i-1] < nums[j]) {
+                    swap(nums[i-1], nums[j]);
+                    break;
+                }
+            }
+        }
     }
 };
