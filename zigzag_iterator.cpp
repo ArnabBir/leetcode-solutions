f6862576e778927553fb26325a71c29e3f990c65
Arnab Bir <arnabbir@gmail.com>
2023-09-08 01:12:47 +0530

    Create zigzag_iterator.cpp

---
+++ zigzag_iterator.cpp
@@ -0,0 +1,45 @@
+// Link : https://leetcode.com/problems/zigzag-iterator
+
+class ZigzagIterator {
+    int i1, i2;
+    int n1, n2;
+    vector<int> v1;
+    vector<int> v2;
+    int curr;
+public:
+    ZigzagIterator(vector<int>& a1, vector<int>& a2) {
+        this->i1 = 0;
+        this->i2 = 0;
+        this->v1 = a1;
+        this->v2 = a2;
+        this->n1 = (int) v1.size();
+        this->n2 = (int) v2.size();
+        this->curr = (n1 == 0) ? 2 : 1;
+    }
+
+    int next() {
+        if (curr == 1) {
+            if (i2 < n2) {
+                curr = 2;
+            }
+            cout<<v1[i1]<<endl;
+            return v1[i1++];
+        } else {
+            if (i1 < n1) {
+                curr = 1;
+            }
+            cout<<v2[i2]<<endl;
+            return v2[i2++];
+        }
+    }
+
+    bool hasNext() {
+        return !(i1 == n1 && i2 == n2);
+    }
+};
+
+/**
+ * Your ZigzagIterator object will be instantiated and called as such:
+ * ZigzagIterator i(v1, v2);
+ * while (i.hasNext()) cout << i.next();
+ */
